6e11e360314b02536a5e86087c9a5e52db20506b
Trevor Snow <trevor.snow@deo.myflorida.com>
2015-06-05 11:52:02 -0400

    Added Flask extensions to handle logged in users, and to handle authentication.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import Login Manager
+from flask.ext.openid import OpenID
+from config import basedir
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
 
-from app import views, models
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
