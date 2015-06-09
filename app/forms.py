475c94eca773d44aea237e141156031e0d5265be
Trevor Snow <Trevor.Snow@deo.myflorida.com>
2015-06-09 16:12:03 -0400

    Form constructor takes 'original_nickname' as new argument. New 'validate' method evaluates if nickname has changed or not. If it hasn't changed it accepts. If change, checks if nickname already exists in database.

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length
-
+from .models import User
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
@@ -11,4 +11,19 @@ class LoginForm(Form):
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
-   
+  
+	def __init__(self, original_nickname, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.original_nickname = original_nickname
+
+	def validate(self):
+		if not Form.validate(self):
+			return False
+		if self.nickname.data == self.original_nickname:
+			return True
+		user = User.query.filter_by(nickname=self.nickname.data).first()
+		if user != None:
+			self.nickname.errors.append('This nickname is already in use. Please choose another one.')
+			return False
+		return True
+ 
