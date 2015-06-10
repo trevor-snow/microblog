f78cc09371f6be7eca8e5a32cb2dcb1124c828e3
Trevor Snow <Trevor.Snow@deo.myflorida.com>
2015-06-10 08:58:47 -0400

    updated

--- app/forms.py
+++ app/forms.py
@@ -4,14 +4,13 @@ from wtforms.validators import DataRequired, Length
 from .models import User
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
-
+	openid = StringField('openid', validators=[DataRequired()])
+	remember_me = BooleanField('remember_me', default=False)
 
 class EditForm(Form):
-    nickname = StringField('nickname', validators=[DataRequired()])
-    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
-  
+	nickname = StringField('nickname', validators=[DataRequired()])
+	about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
 	def __init__(self, original_nickname, *args, **kwargs):
 		Form.__init__(self, *args, **kwargs)
 		self.original_nickname = original_nickname
@@ -22,8 +21,9 @@ class EditForm(Form):
 		if self.nickname.data == self.original_nickname:
 			return True
 		user = User.query.filter_by(nickname=self.nickname.data).first()
-		if user != None:
-			self.nickname.errors.append('This nickname is already in use. Please choose another one.')
+		if user is not None:
+			self.nickname.errors.append('This nickname is already in use. '
+										'Please choose another one.')
 			return False
 		return True
  
